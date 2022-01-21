bd16498a2a72de4465c35539539ba4629042237e
dm4nk <dim.xx2011@yandex.ru>
2022-01-21 17:17:21 +0400

    Initial commit

--- src/test/java/com/dm4nk/track_library_vaadin/TrackLibraryApplicationTests.java
+++ src/test/java/com/dm4nk/track_library_vaadin/TrackLibraryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CreditCalculatorApplicationTests {
+class TrackLibraryApplicationTests {
 
     @Test
     void contextLoads() {
