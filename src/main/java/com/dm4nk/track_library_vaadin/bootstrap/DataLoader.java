407da6f439e55b1173036563f2afc6a560fca2b3
dm4nk <dim.xx2011@yandex.ru>
2022-01-24 11:42:10 +0400

    Added actual tracks in Bootstrap

--- src/main/java/com/dm4nk/track_library_vaadin/bootstrap/DataLoader.java
+++ src/main/java/com/dm4nk/track_library_vaadin/bootstrap/DataLoader.java
@@ -43,51 +43,60 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     private List<Track> getTracks() throws IOException {
 
+        String filePathMontero = "src/main/resources/static/tracks/MONTERO.wav";
+        byte[] dataMontero = Files.readAllBytes(Paths.get(filePathMontero));
         Track track1 = Track.builder()
                 .name("Montero")
                 .author("Lil Nas X")
                 .album("Montero")
                 .duration(LocalTime.of(0, 2, 18))
                 .genre(genreRepository.findByName("rap").orElse(null))
+                .track(ByteArrayToWrappedByteArray.convert(dataMontero))
                 .build();
 
+        String filePathCosmos = "src/main/resources/static/tracks/Космос.wav";
+        byte[] dataCosmos = Files.readAllBytes(Paths.get(filePathCosmos));
         Track track2 = Track.builder()
                 .name("Космос")
                 .author("Лизер")
                 .album("Молодость, Ч.1")
                 .duration(LocalTime.of(0, 3, 40))
                 .genre(genreRepository.findByName("русская").orElse(null))
+                .track(ByteArrayToWrappedByteArray.convert(dataCosmos))
                 .build();
 
+        String filePathRevolution = "src/main/resources/static/tracks/Score.wav";
+        byte[] dataRevolution = Files.readAllBytes(Paths.get(filePathRevolution));
         Track track3 = Track.builder()
                 .name("Revolution")
                 .author("The Score")
                 .album("Atlas")
                 .duration(LocalTime.of(0, 3, 52))
                 .genre(genreRepository.findByName("pop").orElse(null))
+                .track(ByteArrayToWrappedByteArray.convert(dataRevolution))
                 .build();
 
+        String filePathDriveway = "src/main/resources/static/tracks/driveway.wav";
+        byte[] dataDriveway = Files.readAllBytes(Paths.get(filePathDriveway));
         Track track4 = Track.builder()
                 .name("deiveway")
                 .author("Lil Peep")
                 .album("crybaby")
                 .duration(LocalTime.of(0, 2, 39))
                 .genre(genreRepository.findByName("rap").orElse(null))
+                .track(ByteArrayToWrappedByteArray.convert(dataDriveway))
                 .build();
 
-        String filePath = "src/main/resources/static/tracks/Score.wav";
-
-        byte[] data = Files.readAllBytes(Paths.get(filePath));
-
+        String filePathRevenge = "src/main/resources/static/tracks/Score.wav";
+        byte[] dataRevenge = Files.readAllBytes(Paths.get(filePathRevenge));
         Track track5 = Track.builder()
                 .name("Revenge")
                 .author("XXXTENTACION")
                 .album("Revenge")
                 .duration(LocalTime.of(0, 2, 1))
                 .genre(genreRepository.findByName("rap").orElse(null))
-                .track(ByteArrayToWrappedByteArray.convert(data))
+                .track(ByteArrayToWrappedByteArray.convert(dataRevenge))
                 .build();
-
 
         return new LinkedList<>(Arrays.asList(track1, track2, track3, track4, track5));
     }
